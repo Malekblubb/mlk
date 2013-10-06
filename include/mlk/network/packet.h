380d7ad455f15794c415a188d5e30d0de2dafc82
Christoph Malek <malekchristoph@gmail.com>
2013-10-06 15:34:20 +0200

    packet-vector wrapper

--- include/mlk/network/packet.h
+++ include/mlk/network/packet.h
@@ -7,11 +7,35 @@
 #define MLK_NETWORK_PACKET_H
 
 
+#include <vector>
+
+
 namespace mlk
 {
 	namespace ntw
 	{
+		class packet
+		{
+			using data_c = std::vector<unsigned char>;
 
+			data_c m_data;
+
+		public:
+			packet(const data_c& data) :
+				m_data(data)
+			{ }
+
+			data_c data() const noexcept {return m_data;}
+
+			void clear() {m_data.clear();}
+			void add(const data_c& data)
+			{
+				if(data.size() <= 0)
+					return;
+
+				m_data.insert(m_data.end(), data.begin(), data.end());
+			}
+		};
 	}
 }
 
