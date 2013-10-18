c5db2f0ba330f61845ffab19eaa95801195d9f4f
Christoph Malek <malekchristoph@gmail.com>
2013-10-18 20:37:59 +0200

    added mlk::cnt::vec_cmp<T>(int, int, const std::vector<T>&, const std::vector<T>&

--- include/mlk/containers/container_utl.h
+++ include/mlk/containers/container_utl.h
@@ -19,6 +19,20 @@ namespace mlk
 		{
 			return (index >= vec.size()) || (index < 0);
 		}
+
+		// compare lhs and rhs, starting from 'from' end by 'to'
+		template<typename T>
+		bool vec_cmp(int from, int to, const std::vector<T>& lhs, const std::vector<T>& rhs)
+		{
+			if(is_out_of_bounds(lhs, to) || is_out_of_bounds(rhs, to))
+				return false;
+
+			for(int i{from}; i <= to; ++i)
+				if(rhs[i] != lhs[i])
+					return false;
+
+			return true;
+		}
 	}
 }
 
