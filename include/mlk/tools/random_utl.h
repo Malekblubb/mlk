268da4cf6d253b2eff2d6290085650c45442e78a
Christoph Malek <malekchristoph@gmail.com>
2013-11-18 20:23:51 +0100

    added random generator

---
+++ include/mlk/tools/random_utl.h
@@ -0,0 +1,38 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef MLK_TOOLS_RANDOM_UTL_H
+#define MLK_TOOLS_RANDOM_UTL_H
+
+
+#include <mlk/time/time_utl.h>
+
+#include <random>
+
+
+namespace mlk
+{
+	namespace internal
+	{
+		// one instance per run
+		inline std::mt19937& rnd_engine_i()
+		{
+			static std::mt19937 rnd_engine{static_cast<unsigned long>(mlk::tm::time_stmp<std::chrono::nanoseconds>())}; // seed with current nanoseconds
+			return rnd_engine;
+		}
+	}
+
+	template<typename T = int>
+	T rnd(T min, T max)
+	{
+		if(min > max)
+			return 0;
+		return std::uniform_int_distribution<T>{min, max}(internal::rnd_engine_i());
+	}
+}
+
+
+
+#endif // MLK_TOOLS_RANDOM_UTL_H
--- include/mlk/tools/tools.h
+++ include/mlk/tools/tools.h
@@ -8,6 +8,7 @@
 
 
 #include "enum_utl.h"
+#include "random_utl.h"
 #include "stl_string_utl.h"
 #include "type_utl.h"
 
