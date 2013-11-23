34170628f67ffe36d9fe634c1b904b5f905f5368
Christoph Malek <malekchristoph@gmail.com>
2013-11-23 11:28:54 +0100

    added test network

---
+++ test/test_network.cpp
@@ -0,0 +1,20 @@
+#include <mlk/network/network.h>
+
+int main()
+{
+	// create a udp socket
+	mlk::ntw::sock<mlk::ntw::sock_type::udp, false> udp_sock; // the boolean sets blocking mode(true) or non-blocking mode(false)
+	mlk::ntw::ip_address to_addr{"my_address.com:my_port"}; // <- this will look up the ip from host 'my_address.com'
+	// possible alternative: addr{"192.168.1.1, 80, false"}; <- this will NOT look up the ip address
+	mlk::data_packet data{'r', 'e', 'q', 'u', 'e', 's', 't'};
+
+	udp_sock.send(to_addr, data); // send data to host
+
+	mlk::ntw::ip_address from_addr;
+	udp_sock.recv(from_addr, data, 1024); // better: loop this recv when mode is non-blocking
+
+	if(!udp_sock.error())
+		std::cout << data.data() << std::endl; // display the got data response
+
+	// TCP isnt implemented yet
+}
