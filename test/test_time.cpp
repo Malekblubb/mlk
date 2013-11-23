09e2fa6688aa2d155144bcf3f467e9c759d92897
Christoph Malek <malekchristoph@gmail.com>
2013-11-23 12:17:28 +0100

    added test time

---
+++ test/test_time.cpp
@@ -0,0 +1,24 @@
+#include <mlk/time/time.h>
+
+#include <iostream>
+
+
+int main()
+{
+    auto time_now(mlk::tm::time_stmp()); // get current time as long long; f.e.: 1385206783049705
+  
+  
+    auto start(mlk::tm::time_pnt());
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // do work
+    
+    auto end(mlk::tm::time_pnt());
+
+    
+    std::cout << mlk::tm::duration(start, end) << std::endl; // prints 5000
+    // use mlk::tm::duration<std::chrono::microseconds>(start, end) to get microseconds etc..
+    std::cout << mlk::tm::timed_out(start, 1000) << std::endl; // prints 1 because the duration of start and now is higher than 1000
+    
+    
+    std::cout << mlk::tm::time_str() << std::endl; // prints the current time as string; f.e.: Sat Nov 32 12:13:44 2013
+}
