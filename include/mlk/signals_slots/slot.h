93fbb1a2f3bca86e85bbe365cd1c6e9a087a5360
Christoph Malek <malekchristoph@gmail.com>
2013-12-26 18:54:57 +0100

    take args as const ref

--- include/mlk/signals_slots/slot.h
+++ include/mlk/signals_slots/slot.h
@@ -52,14 +52,14 @@ namespace mlk
 		int num_args() const noexcept override
 		{return m_num_args;}
 
-		void operator()(T... args)
+		void operator()(const T&... args)
 		{this->call_funcs(args...);}
 
 	private:
 		bool has_funcs() const noexcept
 		{return !m_funcs.empty();}
 
-		void call_funcs(T... args)
+		void call_funcs(const T&... args)
 		{
 			if(this->has_funcs())
 				for(auto& a : m_funcs)
