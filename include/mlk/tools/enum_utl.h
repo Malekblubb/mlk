c9fa3091b776eb9cf859c79addd09fe9780becb1
Christoph Malek <malekchristoph@gmail.com>
2013-12-26 20:35:04 +0100

    dont pass little amount of bytes via const ref

--- include/mlk/tools/enum_utl.h
+++ include/mlk/tools/enum_utl.h
@@ -16,12 +16,12 @@ namespace mlk
 	{
 		// returns a casts of (enum member) param "enumMember" to its enum type
 		template<typename enum_value, typename enum_type = typename std::underlying_type<enum_value>::type>
-		constexpr enum_type to_enum_type(const enum_value& enum_member)
+		constexpr enum_type to_enum_type(enum_value enum_member)
 		{return static_cast<enum_type>(enum_member);}
 
 		// returns a cast of (enum member) param "enumMember" to an int
 		template<typename int_type = int, typename enum_value>
-		constexpr int_type to_int(const enum_value& enum_member)
+		constexpr int_type to_int(enum_value enum_member)
 		{return static_cast<int_type>(enum_member);}
 	}
 }
