f7e93a7558d4c0344d8583901ac48232d35510d5
Christoph Malek <malekchristoph@gmail.com>
2014-01-19 11:39:30 +0100

    added floating point random numbers

--- include/mlk/tools/random_utl.h
+++ include/mlk/tools/random_utl.h
@@ -10,6 +10,7 @@
 #include <mlk/time/time_utl.h>
 
 #include <random>
+#include <type_traits>
 
 
 namespace mlk
@@ -22,15 +23,35 @@ namespace mlk
 			static std::mt19937 rnd_engine{mlk::tm::time_stmp<std::chrono::nanoseconds>()}; // seed with current nanoseconds
 			return rnd_engine;
 		}
+
+		template<typename T, bool integral>
+		struct rnd_helper
+		{
+			T operator()(T min, T max)
+			{
+				if(min > max)
+					throw std::range_error{"'min' must be less than 'max'"};
+				return std::uniform_int_distribution<T>{min, max}(internal::rnd_engine_i());
+			}
+		};
+
+		template<typename T>
+		struct rnd_helper<T, false>
+		{
+			static_assert(std::is_floating_point<T>(), "T must be a floating point type");
+
+			T operator()(T min, T max)
+			{
+				if(min > max)
+					throw std::range_error{"'min' must be less than 'max'"};
+				return std::uniform_real_distribution<T>{min, max}(internal::rnd_engine_i());
+			}
+		};
 	}
 
 	template<typename T = int>
 	T rnd(T min, T max)
-	{
-		if(min > max)
-			return 0;
-		return std::uniform_int_distribution<T>{min, max}(internal::rnd_engine_i());
-	}
+	{return internal::rnd_helper<T, std::is_integral<T>::value>{}(min, max);}
 }
 
 
