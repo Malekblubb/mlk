525f928b83528814b4ea9d4a62e3a52959460257
Christoph Malek <malekchristoph@gmail.com>
2014-01-28 22:08:57 +0100

    adapted test_filesystem.cpp

--- test/test_filesystem.cpp
+++ test/test_filesystem.cpp
@@ -9,6 +9,7 @@
 int main()
 {
 	mlk::fs::fs_handle<mlk::fs::fs_type::file> my_file{"/path/to/file"};
+	// shortcut: mlk::fs::file_handle
 
 	if(my_file.exists())
 		return 1;
@@ -21,7 +22,13 @@ int main()
 
 
 	mlk::fs::fs_handle<mlk::fs::fs_type::dir> my_dir{"/path/to/dir"};
+	// shortcut: mlk::fs::dir_handle
 
 	if(!my_dir.exists())
 		my_dir.create();
+
+	// get contents of dir:
+	auto content(my_dir.get_content<true>()); // true: recursive or not
+	for(auto& a : content) // 'a' is from type 'mlk::fs::dir_item'
+		std::cout << a.path << std::endl;
 }
