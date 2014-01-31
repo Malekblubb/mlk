3fc02144bedbff4b4a25ebec5b31b4dddeef3eb7
Christoph Malek <malekchristoph@gmail.com>
2014-01-31 19:33:08 +0100

    added mlk::parse_bool(...)

--- include/mlk/tools/utils.h
+++ include/mlk/tools/utils.h
@@ -7,9 +7,11 @@
 #define MLK_TOOLS_UTILS_H
 
 
-#include <iostream>
-#include <string>
+#include <algorithm>
 #include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <string>
 
 
 namespace mlk
@@ -19,6 +21,17 @@ namespace mlk
 	template<typename T = std::ostream>
 	void exit_with(const std::string& reason, T& os = std::cerr)
 	{os << reason; std::exit(EXIT_FAILURE);}
+
+	bool parse_bool(const std::string& str)
+	{
+		auto cpy(str);
+		mlk::stl_string::erase_all(' ', cpy);
+		if(cpy.empty())
+			throw std::invalid_argument{"cant parse empty string"};
+
+		std::transform(std::begin(cpy), std::end(cpy), std::begin(cpy), [](char c){return std::tolower(c);});
+		return cpy[0] == 't' || cpy[0] == '1';
+	}
 }
 
 
