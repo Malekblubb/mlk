b0d1c3cd21cbb5bec729f7dad6a87a379394f775
Christoph Malek <malekchristoph@gmail.com>
2014-02-16 11:42:05 +0100

    added a basic tcp impl

--- include/mlk/network/tcp.h
+++ include/mlk/network/tcp.h
@@ -15,29 +15,51 @@ namespace mlk
 	namespace ntw
 	{
 		template<bool blocking>
-		class sock<sock_type::tcp, blocking>;
+		class sock<sock_type::tcp, blocking> : public internal::sock_base
+		{
+			ip_address m_targetaddress;
+			ip_address m_destaddress;
 
-//		template<bool blocking>
-//		class sock<sock_type::tcp, blocking> : public internal::sock_base
-//		{
-//			ip_address m_address;
+		public:
+			sock(const ip_address& target, const ip_address& dest = {"0.0.0.0:0", false}) :
+				internal::sock_base{internal::get_sock(SOCK_STREAM, 0)},
+				m_targetaddress{target},
+				m_destaddress{dest}
+			{
+				this->set_blocking(blocking);
+				internal::set_sock_opt(m_sock, SO_REUSEADDR);
+				this->init();
+			}
 
-//		public:
-//			sock() :
-//				internal::sock_base(internal::get_sock(SOCK_STREAM, 0))
-//			{this->set_blocking(blocking);}
+			ssize_t send(const data_packet& data)
+			{return send(m_sock, data.data(), data.size());}
 
-//			bool connect(const ip_address& to)
-//			{
+			ssize_t recv(data_packet& data, size_t max_len)
+			{return recv(m_sock, data.data(), data.size());}
 
-//			}
-
-//			ssize_t send(const data_packet& data)
-//			{}
-
-//			ssize_t recv(data_packet &data, size_t max_len)
-//			{}
-//		};
+		private:
+			void init()
+			{
+				this->reset_error();
+				if(m_sock < 0)
+				{
+					m_error = true;
+					return;
+				}
+				auto dest_si(internal::to_sockaddr_in(m_destaddress.ip(), m_destaddress.port<std::uint16_t>()));
+				if(bind(m_sock, reinterpret_cast<sockaddr*>(&dest_si), sizeof(sockaddr)) != 0)
+				{
+					m_error = true;
+					return;
+				}
+				auto target_si(internal::to_sockaddr_in(m_targetaddress.ip(), m_targetaddress.port<std::uint16_t>()));
+				if(connect(m_sock, reinterpret_cast<sockaddr*>(&target_si), sizeof(sockaddr)) != 0)
+				{
+					m_error = true;
+					return;
+				}
+			}
+		};
 	}
 }
 
