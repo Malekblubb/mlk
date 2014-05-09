b78a46d7059cff279001177cd1a1cb07d7f08f78
Christoph Malek <malekchristoph@gmail.com>
2014-05-09 20:25:47 +0200

    added image

---
+++ include/mlk/graphics/image.h
@@ -0,0 +1,60 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef MLK_GRAPHICS_IMAGE_H
+#define MLK_GRAPHICS_IMAGE_H
+
+
+#include <mlk/log/log.h>
+#include <mlk/types/types.h>
+
+#include <pnglite.h>
+
+
+namespace mlk
+{
+	namespace gcs
+	{
+		class image
+		{
+			mlk::data_packet m_raw_pixels;
+
+			png_t m_pnghandle;
+
+		public:
+			image()
+			{png_init(0, 0);}
+
+			image(const mlk::data_packet& raw_pixels, mlk::uint width, mlk::uint height) :
+				image{ },
+				m_raw_pixels{raw_pixels}
+			{ }
+
+			~image()
+			{png_close_file(&m_pnghandle);}
+
+
+			bool save(const std::string& file)
+			{
+				auto error(0);
+				if((error = png_open_file_write(&m_pnghandle, file.c_str())) != PNG_NO_ERROR)
+				{
+					mlk::lerr()["mlk::gcs::image"] << "png_open_file_write failed: " << error;
+					return false;
+				}
+
+				if(m_raw_pixels.empty())
+					mlk::lout("mlk::gcs::image") << "warning: pixelbuffer is empty";
+
+				png_set_data(&m_pnghandle, width, height, 8, PNG_TRUECOLOR_ALPHA, m_raw_pixels.data());
+				png_close_file(&m_pnghandle);
+			}
+		};
+	}
+}
+
+
+#endif // MLK_GRAPHICS_IMAGE_H
+
