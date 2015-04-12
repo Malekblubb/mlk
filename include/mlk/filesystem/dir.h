ea61c413a4f32084caf58a1ddbebc07fe449b1a1
Malekblubb <malekchristoph@gmail.com>
2015-04-12 15:20:53 +0200

    fixed macro

--- include/mlk/filesystem/dir.h
+++ include/mlk/filesystem/dir.h
@@ -21,39 +21,39 @@
 
 namespace mlk
 {
-	namespace fs
-	{
-		namespace dir
-		{
-			// checks if a dir exists
-			inline bool exists(const std::string& path)
-			{
-                struct stat s{};
-				stat(path.c_str(), &s);
-				return s.st_mode & S_IFDIR;
-			}
-
-			inline bool create(const std::string& path)
+    namespace fs
+    {
+        namespace dir
+        {
+            // checks if a dir exists
+            inline bool exists(const std::string& path)
             {
-#ifdef MLK_LINUX || defined MLK_OS_MAC
-				return mkdir(path.c_str(), 0755) != -1;
+                struct stat s{};
+                stat(path.c_str(), &s);
+                return s.st_mode & S_IFDIR;
+            }
+            
+            inline bool create(const std::string& path)
+            {
+#if defined(MLK_LINUX) || defined (MLK_OS_MAC)
+                return mkdir(path.c_str(), 0775) != -1;
 #elif defined MLK_WIN
-				return mkdir(path.c_str()) != -1;
+                return mkdir(path.c_str()) != -1;
 #endif
                 return false;
             }
-			
-			inline bool remove(const std::string& path)
-			{
-#ifdef MLK_LINUX || defined MLK_OS_MAC
-				return rmdir(path.c_str()) != -1;
+            
+            inline bool remove(const std::string& path)
+            {
+#if defined(MLK_LINUX)
+                return rmdir(path.c_str()) != -1;
 #elif defined MLK_WIN
-				return _rmdir(path.c_str()) != -1;
+                return _rmdir(path.c_str()) != -1;
 #endif
                 return false;
-			}
-		}
-	}
+            }
+        }
+    }
 }
 
 
