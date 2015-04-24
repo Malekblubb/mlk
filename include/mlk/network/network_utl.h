69400fa773089dcc4fb59d114e5b17b348d16014
Malekblubb <malekchristoph@gmail.com>
2015-04-24 13:29:05 +0200

    mac

--- include/mlk/network/network_utl.h
+++ include/mlk/network/network_utl.h
@@ -32,116 +32,122 @@ using socklen_t = mlk::uint;
 
 namespace mlk
 {
-	namespace ntw
-	{
-		namespace internal
-		{
-			// low level ntw stuff
-			inline void init_ntw()
-			{
+    namespace ntw
+    {
+        namespace internal
+        {
+            // low level ntw stuff
+            inline void init_ntw()
+            {
 #ifdef MLK_WIN
-				// win sux
-				WSADATA data;
-				WORD version{MAKEWORD(2, 2)};
-				WSAStartup(version, &data);
+                // win sux
+                WSADATA data;
+                WORD version{MAKEWORD(2, 2)};
+                WSAStartup(version, &data);
 #endif
-			}
+            }
 
-			inline void clean_ntw()
-			{
+            inline void clean_ntw()
+            {
 #ifdef MLK_WIN
-				WSACleanup();
+                WSACleanup();
 #endif
-			}
+            }
 
-			inline int get_sock(int type, int proto)
-			{
-				init_ntw();
-				return socket(AF_INET, type, proto);
-			}
+            inline int get_sock(int type, int proto)
+            {
+                init_ntw();
+                return socket(AF_INET, type, proto);
+            }
 
-			inline void close_sock(int sock)
-			{
+            inline void close_sock(int sock)
+            {
 #if defined(MLK_LINUX) || defined(MLK_OS_MAC)
-				close(sock);
+                close(sock);
 #elif defined MLK_WIN
-				closesocket(sock);
+                closesocket(sock);
 #endif
-			}
+            }
 
-			inline sockaddr_in to_sockaddr_in(const std::string& ip, uint16_t port)
-			{
+                        inline sockaddr_in to_sockaddr_in(const std::string& ip, uint16_t port)
+            {
+#if defined(MLK_LINUX)
+                                return sockaddr_in{AF_INET, htons(port), {inet_addr(ip.c_str())}, {0}};
+#elif defined MLK_OS_MAC
+                                sockaddr_in result{};
+                                result.sin_family = AF_INET;
+                                result.sin_port = htons(port);
+                                result.sin_addr.s_addr = inet_addr(ip.c_str());
+                                return result;
+#elif defined MLK_WIN
+                sockaddr_in result{0, 0, {0}, {0}};
+                result.sin_family = AF_INET;
+                result.sin_port = htons(port);
+                result.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
+                return result;
+#endif
+            }
+
+            inline auto from_sockaddr_in(const sockaddr_in& sock_addr)
+            -> std::pair<std::string, uint16_t>
+            {return std::make_pair(inet_ntoa(sock_addr.sin_addr), htons(sock_addr.sin_port));}
+
+            inline int bind_sock(int sock, const std::string& ip, const uint16_t port)
+            {
+                auto tmp(to_sockaddr_in(ip, port));
+                return bind(sock, reinterpret_cast<sockaddr*>(&tmp), sizeof tmp);
+            }
+
+            inline void set_blocking(int sock)
+            {
 #if defined(MLK_LINUX) || defined(MLK_OS_MAC)
-				return sockaddr_in{AF_INET, htons(port), {inet_addr(ip.c_str())}, {0}};
+                int op{~O_NONBLOCK};
+                fcntl(sock, F_SETFL, op);
 #elif defined MLK_WIN
-				sockaddr_in result{0, 0, {0}, {0}};
-				result.sin_family = AF_INET;
-				result.sin_port = htons(port);
-				result.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
-				return result;
+                u_long mode{0};
+                ioctlsocket(sock, FIONBIO, &mode);
 #endif
-			}
+            }
 
-			inline auto from_sockaddr_in(const sockaddr_in& sock_addr)
-			-> std::pair<std::string, uint16_t>
-			{return std::make_pair(inet_ntoa(sock_addr.sin_addr), htons(sock_addr.sin_port));}
-
-			inline int bind_sock(int sock, const std::string& ip, const uint16_t port)
-			{
-				auto tmp(to_sockaddr_in(ip, port));
-				return bind(sock, reinterpret_cast<sockaddr*>(&tmp), sizeof tmp);
-			}
-
-			inline void set_blocking(int sock)
-			{
+            inline void set_no_blocking(int sock)
+            {
 #if defined(MLK_LINUX) || defined(MLK_OS_MAC)
-				int op{~O_NONBLOCK};
-				fcntl(sock, F_SETFL, op);
+                fcntl(sock, F_SETFL, O_NONBLOCK);
 #elif defined MLK_WIN
-				u_long mode{0};
-				ioctlsocket(sock, FIONBIO, &mode);
+                u_long mode{1};
+                ioctlsocket(sock, FIONBIO, &mode);
 #endif
-			}
+            }
 
-			inline void set_no_blocking(int sock)
-			{
+            inline void set_sock_opt(int& sock, int opt)
+            {const char* optval{"1"}; setsockopt(sock, SOL_SOCKET, opt, optval, sizeof optval);}
+
+            inline char* get_sock_opt(const int& sock, int opt)
+            {
+                char* result{0};
 #if defined(MLK_LINUX) || defined(MLK_OS_MAC)
-				fcntl(sock, F_SETFL, O_NONBLOCK);
+                socklen_t len{sizeof result};
 #elif defined MLK_WIN
-				u_long mode{1};
-				ioctlsocket(sock, FIONBIO, &mode);
+                int len{sizeof result};
 #endif
-			}
+                getsockopt(sock, SOL_SOCKET, opt, result, &len);
+                return result;
+            }
 
-			inline void set_sock_opt(int& sock, int opt)
-			{const char* optval{"1"}; setsockopt(sock, SOL_SOCKET, opt, optval, sizeof optval);}
+            inline std::string ip_from_host(const std::string& host)
+            {
+                hostent* h{gethostbyname(host.c_str())};
+                if(h == nullptr)
+                    return "";
 
-			inline char* get_sock_opt(const int& sock, int opt)
-			{
-				char* result{0};
-#if defined(MLK_LINUX) || defined(MLK_OS_MAC)
-				socklen_t len{sizeof result};
-#elif defined MLK_WIN
-				int len{sizeof result};
-#endif
-				getsockopt(sock, SOL_SOCKET, opt, result, &len);
-				return result;
-			}
+                in_addr* a{(in_addr*)h->h_addr_list[0]};
+                if(a == nullptr)
+                    return "";
 
-			inline std::string ip_from_host(const std::string& host)
-			{
-				hostent* h{gethostbyname(host.c_str())};
-				if(h == nullptr)
-					return "";
-
-				in_addr* a{(in_addr*)h->h_addr_list[0]};
-				if(a == nullptr)
-					return "";
-
-				return inet_ntoa(*a);
-			}
-		}
-	}
+                return inet_ntoa(*a);
+            }
+        }
+    }
 }
 
 
