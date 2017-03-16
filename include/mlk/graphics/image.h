78e8b7642fe7b5684de191cd2f5d5c10ae2b1b16
Christoph Malek <malekchristoph@gmail.com>
2017-03-16 19:27:02 +0100

    image: fix

--- include/mlk/graphics/image.h
+++ include/mlk/graphics/image.h
@@ -17,10 +17,10 @@ namespace mlk
 	{
 		class image
 		{
+			png_t m_pnghandle;
 			mlk::data_packet m_raw_pixels;
 			mlk::uint m_width, m_height;
-
-			png_t m_pnghandle;
+			bool m_open{false};
 
 		public:
 			image(const mlk::data_packet& raw_pixels, mlk::uint width,
@@ -30,10 +30,14 @@ namespace mlk
 				png_init(nullptr, nullptr);
 			}
 
-			~image() { png_close_file(&m_pnghandle); }
+			~image()
+			{
+				if(m_open) png_close_file(&m_pnghandle);
+			}
 
 			bool save(const std::string& file)
 			{
+				m_open = true;
 				auto error(0);
 				if((error = png_open_file_write(&m_pnghandle, file.c_str())) !=
 				   PNG_NO_ERROR)
@@ -50,6 +54,7 @@ namespace mlk
 				png_set_data(&m_pnghandle, m_width, m_height, 8,
 							 PNG_TRUECOLOR_ALPHA, m_raw_pixels.data());
 				png_close_file(&m_pnghandle);
+				m_open = false;
 				return true;
 			}
 		};
